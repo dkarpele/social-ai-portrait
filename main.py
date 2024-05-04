1dd0727e4b39399fe531693d7f1b3f34d5c7d3bd
Denis Karpelevich <dkarpele@redhat.com>
2024-05-04 23:27:53 +0200

    Publish to Heroku

---
+++ main.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+import os
+
+token = os.environ['TOKEN']
+url = os.environ['URL']
+port = int(os.environ.get('PORT'), 5000)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="I'm a bot, please talk to me!")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_webhook(listen='0.0.0.0',
+                            port=port,
+                            secret_token=token,
+                            webhook_url=url + token)
+
