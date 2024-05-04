57661be933584bfc90a5d3c47e6eef8e716291fc
Denis Karpelevich <dkarpele@redhat.com>
2024-05-04 23:48:02 +0200

    Fix bad port 2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 token = os.environ['TOKEN']
 url = os.environ['URL']
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8443))
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -21,6 +21,5 @@ if __name__ == '__main__':
 
     application.run_webhook(listen='0.0.0.0',
                             port=port,
-                            secret_token=token,
-                            webhook_url=url + token)
+                            webhook_url=url)
 
